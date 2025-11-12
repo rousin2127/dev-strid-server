1a2b8e8516b75d93e0afba411790b9cc674c58a8
rousin2127 <rousinmd@gmail.com>
2025-11-12 10:03:53 +0600

    connect Database with server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors= require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -8,10 +9,39 @@ const port = process.env.PORT || 3000
 app.use(cors());
 app.use(express.json())
 
+//user and pass 
+
+//rHtfAverbFyZe4st
+
+const uri = "mongodb+srv://devStridUser:rHtfAverbFyZe4st@cluster0.ijc2zmy.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
 
 app.get('/', (req, res) => {
   res.send('DevStrid server is Running')
 })
+
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`DevStrid server is Running on port ${port}`)
